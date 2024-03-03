766f38ae4562e380ca3957fa1bc52c12c5facec9
pradHN1009 <pradyumnahn@gmail.com>
2024-03-03 12:30:32 +0530

    Using new databse in mongodb and using async await for user creation

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoURI = "mongodb://localhost:27017";
+mongoURI = "mongodb://localhost:27017/Notero";
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI).then(() => {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const User = require('../models/User')
+const { body, validationResult } = require('express-validator')
 
 router = express.Router()
 
-router.post('/', (req, res) => {
-    console.log(req.body)
-    const user = User(req.body)
-    user.save()
-    res.json([])
+router.post('/', body('name', "Enter name greater than 3 chars").isLength({ min: 3 }), body('email', "Enter valid email").isEmail(), body('password', "Enter password greater than 5 chars").isLength({ min: 5 }), async(req, res) => {
+    const result = validationResult(req)
+    if (result.isEmpty()) {
+        console.log(req.body)
+        try {
+            const user = await User.create(req.body)
+            res.json(user)
+        } catch (err) {
+            console.log(err)
+            res.send("Error in creating user")
+        }
+    } else {
+        res.send({ errors: result.array() });
+    }
 })
-
 module.exports = router
